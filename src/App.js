de09788cedafbbea9788778c24ea6694e71e198c
Alfie <alfiecollins44@gmail.com>
2024-05-18 22:17:07 +0100

    404 page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Faq from './pages/help/Faq';
 //layouts 
 import RootLayout from './layouts/RootLayout'
 import HelpLayout from './layouts/HelpLayout';
+import NotFound from './pages/NotFound';
 
 const  router = createBrowserRouter(
   createRoutesFromElements(
@@ -20,6 +21,7 @@ const  router = createBrowserRouter(
           <Route path='faq' element={<Faq />}/>
           <Route path='contact' element={<Contact />}/>
       </Route>
+          <Route path='*' element={<NotFound />}/>
     </Route>
   )
 )
